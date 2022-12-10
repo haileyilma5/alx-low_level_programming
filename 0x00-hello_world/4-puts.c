32e3530d69efd805a37c0335247edecaaf4eabe9
haileyilma5 <hailetube11@gmail.com>
2022-12-10 06:56:23 -0800

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,12 +6,10 @@
 
 int main (void)
 {
-	        puts("\"Programming is like building a multilingual puzzle");
-		        return (0);
+	 puts("\"Programming is like building a multilingual puzzle");
+	         return (0);
+
 }
-
-
-
 
 
 
