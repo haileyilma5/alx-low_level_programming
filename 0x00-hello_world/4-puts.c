a99fbca7252fccad55ab579a226b8602d6bf8b9f
haileyilma5 <hailetube11@gmail.com>
2022-12-10 02:48:10 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 #include <studio.h>
-
 /**
- *main - Entry point
- *Return: Always 0 (success)
+ * main - Entry point
+ * Return: Always 0 (success)
  */
-int main(void)
+
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	        puts("\"Programming is like building a multilingual puzzle");
+		        return (0);
 }
+
+
+
+
+
+
+
+
+
+
