c414d2238f263cc66ad3ba15391b4f286189dba5
haileyilma5 <hailetube11@gmail.com>
2022-12-10 01:55:45 -0800

    Task 5

---
+++ 0x00-hello_world/5-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("with proper garammar, but thr outcome is a piece of art, \n");
+	return (0);
+}
